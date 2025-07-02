9d04ecd39f2b9933e2ac8240d77b0599e28d3c8c
Collins <donsmatt44@gmail.com>
2025-07-02 08:38:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>Navbar Component here...</header>
+      <main>
+        <h1>DEVHUB</h1>
+        <p>Sections go here...</p>
+      </main>
+      <footer>Footer component here...</footer>
+    </div>
+  );
+}
+
+export default App;
